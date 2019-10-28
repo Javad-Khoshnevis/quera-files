9abcf1a7bd7b6b0af63797057e5f8f9da0be6a91
Javad-Khoshnevis <j.khoshnevis.80@gmail.com>
2019-10-28 21:11:26 +0330

    i promess I promise this is really DONE / Rooz e Azadi e Bayan

--- rooz e azadi e bayan/solve.py
+++ rooz e azadi e bayan/solve.py
@@ -1,20 +1,32 @@
+#By Javad Khoshnevis
+#Github : Javad-Khoshnevis
+#------------------------------------------------
+
 print ("\033c") #console ro clear mikone !!!!
-try:
-    k = int(input("'K' Ra Vared Konid :"))
-except ValueError:
-    print("Lotfan ADAD Vared Konid!")
-except NameError :
-    print("Lotfan ADAD Vared Konid!")
-except SyntaxError :
-    print("Lotfan ADAD Vared Konid!")
-else:    
-    if k >= 1 and k <= 100 :
-        mod = divmod(k,2)
-        x = mod[1]
-        if x is 0 :
-            print("Payin Barare")
+while 1 :
+    try:
+        k = int(input("'K' Ra Vared Konid :"))
+    except ValueError:
+        print("Lotfan ADAD Vared Konid!")
+    except NameError :
+        print("Lotfan ADAD Vared Konid!")
+    except SyntaxError :
+        print("Lotfan ADAD Vared Konid!")
+    except TypeError :
+        print("Lotfan ADAD Vared Konid!")
+    else:    
+        if k >= 1 and k <= 100 :
+            mod = divmod(k,2)
+            x = mod[1]
+            if x is 0 :
+                print("Payin Barare")
+                break
+            else :
+                print("Bala Barare")
+                break
         else :
-            print("Bala Barare")
-    else :
-        print("invalid number!")
-        exit
+            print("invalid number!")
+
+#------------------------------------------------
+#By Javad Khoshnevis
+#Github : Javad-Khoshnevis
